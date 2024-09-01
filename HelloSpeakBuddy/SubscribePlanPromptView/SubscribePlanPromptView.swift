d2f43f986fdbc290d8fe77d799591550f32e401d
vijay <Vijaychandran.jayachandran@ntucenterprise.sg>
2024-09-01 15:27:22 +0800

    added gradieng BG

--- HelloSpeakBuddy/SubscribePlanPromptView/SubscribePlanPromptView.swift
+++ HelloSpeakBuddy/SubscribePlanPromptView/SubscribePlanPromptView.swift
@@ -9,7 +9,64 @@ import SwiftUI
 import Combine
 
 struct SubscribePlanPromptView: View {
-  var body: some View {
-    return Text("Hello")
+  @State var viewModel: SubscribePlanPromptViewModel
+  let button = Button {
+    
+  } label: {
+    Text("Subscribe")
   }
+  
+  private var viewDidAppearSubject = PassthroughSubject<Void, Never>()
+  private var signupButtonTapSubject = PassthroughSubject<Void, Never>()
+
+  init(viewModel: SubscribePlanPromptViewModel) {
+    self.viewModel = viewModel
+    viewModel.transform(
+      input: SubscribePlanPromptViewModel.Input(
+        viewDidAppear: viewDidAppearSubject.eraseToAnyPublisher(),
+        subscribeButtonTapPublisher: signupButtonTapSubject.eraseToAnyPublisher()))
+  }
+
+  var body: some View {
+    ZStack {
+      makeGradientView().ignoresSafeArea()
+    }.onAppear {
+      viewDidAppearSubject.send()
+    }
+  }
+  
+  var activityIndicator: some View {
+    return ProgressView().progressViewStyle(.circular)
+  }
+  
+}
+
+extension SubscribePlanPromptView {
+  private func makeGradientView() -> some View {
+    let rgbaTop = [213, 210, 255, 255].map { Double($0 / 255.0) }
+    let rgbaBottom = [255, 255, 255, 255].map { Double($0 / 255.0) }
+    return LinearGradient(
+
+      colors: [
+        Color(
+          red: rgbaTop[0],
+          green: rgbaTop[1],
+          blue: rgbaTop[2],
+          opacity: rgbaTop[3]),
+        Color(
+          red: rgbaBottom[0],
+          green: rgbaBottom[1],
+          blue: rgbaBottom[2],
+          opacity: rgbaBottom[3]
+        ),
+      ],
+      startPoint: .top,
+      endPoint: .bottom
+    )
+  }
+}
+#Preview {
+  SubscribePlanPromptView(
+    viewModel: SubscribePlanPromptViewModel()
+  )
 }
