41d65619bc6bb6940b146a3bab43267971be2da2
vijay <Vijaychandran.jayachandran@ntucenterprise.sg>
2024-09-06 19:16:14 +0800

    moved sub views to extension

---
+++ HelloSpeakBuddy/SubscribePlanPromptView/SubscribePlanPromptView+ViewComponents.swift
@@ -0,0 +1,85 @@
+//
+//  SubscribePlanPromptView+ViewComponents.swift
+//  HelloSpeakBuddy
+//
+//  Created by Vijaychandran Jayachandran on 6/9/24.
+//
+
+import SwiftUI
+
+extension SubscribePlanPromptView {
+  
+  func maketitleLabel() -> some View {
+    Text(Constant.pageTitle)
+      .font(
+        .system(
+          size: 36,
+          weight: .bold,
+          design: .default
+        )
+      )
+      .lineSpacing(46.8 - 36.0)
+      .multilineTextAlignment(.center)
+  }
+  
+  
+  func makeGraphWithProttyView() -> some View {
+    GeometryReader { geometry in
+      BarGraphView().background {
+        Image("Protty")
+          .modifier(
+            SpeakBuddy.LivelyAnimation(
+              animate: animateProtty
+            )
+          )
+          .aspectRatio(contentMode: .fit)
+          .frame(width: 187.72, height: 170.0)
+          .offset(
+            CGSize(
+              width: -geometry.size.width * 0.5 - Constant.prottyOffsetFromGraph.width,
+              height: -geometry.size.height * 0.5 - Constant.prottyOffsetFromGraph.height
+            )
+          )
+      }
+    }
+  }
+  
+  func makePromoTextView() -> some View {
+    return VStack {
+      Text(Constant.levelUpText)
+        .font(Font(UIFont(name: "HiraginoSans-W6", size: Constant.levelUpTextPointSize)!))
+        .kerning(-0.57)
+        .lineSpacing(15)
+        .multilineTextAlignment(.center)
+        .frame(maxWidth: .infinity, alignment: .center)
+      Text(Constant.speakBuddyText).modifier(SpeakBuddy.GradientTextModifier())
+    }
+  }
+  
+  func makeSubscribeButton() -> some View {
+    return Button {
+      performSubscribeButtonAction()
+    } label: {
+      Text(Constant.subscribeButtonTitle)
+    }.buttonStyle(
+      SpeakBuddy.ThemeButtonStyle()
+      /* Shadow is handled here here instead of in buttonStyle, to handle case where
+       shadows may not be present at certain placements
+       */
+    ).shadow(
+      color: Constant.subscribeButtonShadowColor,
+      radius: 10.0, x: 0.0, y: 2.0)
+    .padding(.horizontal)
+  }
+  
+  func makeGradientView() -> some View {
+    return LinearGradient(
+      colors: [
+        Constant.backgroundGradientStartColor,
+        Constant.backgroundGradientEndColor
+      ],
+      startPoint: .top,
+      endPoint: .bottom
+    )
+  }
+}
--- HelloSpeakBuddy/SubscribePlanPromptView/SubscribePlanPromptView.swift
+++ HelloSpeakBuddy/SubscribePlanPromptView/SubscribePlanPromptView.swift
@@ -13,7 +13,7 @@ struct SubscribePlanPromptView: View {
   
   private var viewDidAppearSubject = PassthroughSubject<Void, Never>()
   private var signupButtonTapSubject = PassthroughSubject<Void, Never>()
-  @State private var animateProtty = false
+  @State var animateProtty = false
 
   init(viewModel: SubscribePlanPromptViewModel) {
     self.viewModel = viewModel
@@ -77,89 +77,15 @@ struct SubscribePlanPromptView: View {
 }
 
 extension SubscribePlanPromptView {
-  private func maketitleLabel() -> some View {
-    Text(Constant.pageTitle)
-      .font(
-        .system(
-          size: 36,
-          weight: .bold,
-          design: .default
-        )
-      )
-      .lineSpacing(46.8 - 36.0)
-      .multilineTextAlignment(.center)
-  }
-    
-
-  private func makeGraphWithProttyView() -> some View {
-    GeometryReader { geometry in
-      BarGraphView().background {
-        Image("Protty")
-          .modifier(
-            SpeakBuddy.LivelyAnimation(
-              animate: animateProtty
-            )
-          )
-          .aspectRatio(contentMode: .fit)
-          .frame(width: 187.72, height: 170.0)
-          .offset(
-            CGSize(
-              width: -geometry.size.width * 0.5 - Constant.prottyOffsetFromGraph.width,
-              height: -geometry.size.height * 0.5 - Constant.prottyOffsetFromGraph.height
-            )
-          )
-      }
-    }
-  }
-
-  private func makePromoTextView() -> some View {
-    return VStack {
-      Text(Constant.levelUpText)
-        .font(Font(UIFont(name: "HiraginoSans-W6", size: Constant.levelUpTextPointSize)!))
-        .kerning(-0.57)
-        .lineSpacing(15)
-        .multilineTextAlignment(.center)
-        .frame(maxWidth: .infinity, alignment: .center)
-      Text(Constant.speakBuddyText).modifier(SpeakBuddy.GradientTextModifier())
-    }
-  }
-  
-  private func performSubscribeButtonAction() {
+  func performSubscribeButtonAction() {
     vibrateForSubscribeButtonTap()
     animateProtty = true
     DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
       animateProtty = false
     }
   }
-    
-  private func makeSubscribeButton() -> some View {
-    return Button {
-      performSubscribeButtonAction()
-    } label: {
-      Text(Constant.subscribeButtonTitle)
-    }.buttonStyle(
-      SpeakBuddy.ThemeButtonStyle()
-      /* Shadow is handled here here instead of in buttonStyle, to handle case where
-        shadows may not be present at certain placements
-       */
-    ).shadow(
-      color: Constant.subscribeButtonShadowColor,
-      radius: 10.0, x: 0.0, y: 2.0)
-    .padding(.horizontal)
-  }
-  
-  private func makeGradientView() -> some View {
-    return LinearGradient(
-      colors: [
-        Constant.backgroundGradientStartColor,
-        Constant.backgroundGradientEndColor
-      ],
-      startPoint: .top,
-      endPoint: .bottom
-    )
-  }
-  
-  private func vibrateForSubscribeButtonTap() {
+      
+  func vibrateForSubscribeButtonTap() {
     let feedbackGenerator = UIImpactFeedbackGenerator(style: .soft)
     feedbackGenerator.impactOccurred()
   }
