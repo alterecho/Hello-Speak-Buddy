6741a4685260590b38a117a66c5fdf1575d74f09
vijay <Vijaychandran.jayachandran@ntucenterprise.sg>
2024-09-07 16:56:43 +0800

    close button font correction

--- HelloSpeakBuddy/CommonViewComponents/CloseButton.swift
+++ HelloSpeakBuddy/CommonViewComponents/CloseButton.swift
@@ -9,45 +9,61 @@ import SwiftUI
 
 struct CloseButton: View {
   enum Constant {
-    static let closeButtonText = "X"
+    static let closeButtonText = "×"
     static let fontSize: CGFloat = 20
+    static let textColor = Color.fromRGBA256Color(
+      red: 50,
+      green: 53,
+      blue: 55,
+      alpha: 1.0
+    )
+    
+    static let backgroundColor = Color.fromRGBA256Color(
+      red: 255,
+      green: 255,
+      blue: 255,
+      alpha: 1.0
+    )
+    
+    static let shadowColor = Color.fromRGBA256Color(
+      red: 0,
+      green: 0,
+      blue: 0,
+      alpha: 0.15
+    )
+    static let shadowRadius = 10.0
+    static let shadowOffset = CGSize(width: 0.0, height: 2.0)
   }
   
   var body: some View {
     GeometryReader { geometry in
       Text(Constant.closeButtonText)
         .font(
-          .custom("HiraginoSans-W7", size: Constant.fontSize)
+          .custom(
+            "HiraginoSans-W6",
+            size: Constant.fontSize
+          )
         )
-        .multilineTextAlignment(.center)
+        .foregroundStyle(Constant.textColor)
         .frame(
           width: geometry.size.width,
-          height: geometry.size.height
-        ).background(
-          Color.fromRGBA256Color(
-            red: 255,
-            green: 255,
-            blue: 255,
-            alpha: 1.0
-          )
-        ).cornerRadius(geometry.size.height * 0.5)
+          height: geometry.size.height,
+          alignment: .center
+        )
+        .background(Constant.backgroundColor)
+        .cornerRadius(geometry.size.height * 0.5)
         .shadow(
-          color: Color.fromRGBA256Color(
-            red: 0,
-            green: 0,
-            blue: 0,
-            alpha: 0.15
-          ), radius: 10.0
-        ).offset(
-          CGSize(
-            width: 0.0,
-            height: 2.0
-          )
+          color: Constant.shadowColor,
+          radius: Constant.shadowRadius,
+          x: Constant.shadowOffset.width,
+          y: Constant.shadowOffset.height
         )
     }
   }
 }
 
 #Preview {
-  SubscribePlanPromptView(viewModel: SubscribePlanPromptViewModel())
+  SubscribePlanPromptView(
+    viewModel: SubscribePlanPromptViewModel()
+  )
 }
