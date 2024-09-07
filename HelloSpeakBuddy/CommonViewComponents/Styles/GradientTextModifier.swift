40fcda6f1eb857be18481f03c6dcf129730d2525
vijay <Vijaychandran.jayachandran@ntucenterprise.sg>
2024-09-07 17:27:32 +0800

    promo text correction

--- HelloSpeakBuddy/CommonViewComponents/Styles/GradientTextModifier.swift
+++ HelloSpeakBuddy/CommonViewComponents/Styles/GradientTextModifier.swift
@@ -9,30 +9,49 @@ import SwiftUI
 
 extension SpeakBuddy {
   struct GradientTextModifier: ViewModifier {
+    private enum Constant {
+      static let gradientColorStart = Color.fromRGBA256Color(
+        red: 111,
+        green: 212,
+        blue: 255,
+        alpha: 1.0
+      )
+      static let gradientColorEnd = Color.fromRGBA256Color(
+        red: 0,
+        green: 117,
+        blue: 255,
+        alpha: 1.0
+      )
+      
+      static let fontSize: CGFloat = 30.0
+    }
+    
     func body(content: Content) -> some View {
       LinearGradient(
         colors: [
-          Color.fromRGBA256Color(
-            red: 111,
-            green: 212,
-            blue: 255,
-            alpha: 1.0
-          ),
-          Color.fromRGBA256Color(
-            red: 0,
-            green: 117,
-            blue: 255,
-            alpha: 1.0
-          )
+          Constant.gradientColorStart,
+          Constant.gradientColorEnd
         ],
         startPoint: .top,
         endPoint: .bottom
-      ).frame(height: 45.0)
-        .mask {
-          content.font(Font(UIFont(name: "HiraginoSans-W6", size: 30)!))
-            .multilineTextAlignment(.center)
-            .frame(maxWidth: .infinity, alignment: .center)
-        }
+      )
+      .frame(height: 45.0)
+      .mask {
+        content
+          .font(
+            Font(UIFont(name: "HiraginoSans-W6", size: Constant.fontSize)!)
+          )
+          .frame(
+            maxWidth: .infinity,
+            alignment: .center
+          )
+      }
     }
   }
+}
+
+#Preview {
+  SubscribePlanPromptView(
+    viewModel: SubscribePlanPromptViewModel()
+  )
 }
